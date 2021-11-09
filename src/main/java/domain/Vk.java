5cc62bf2fd5561293bdd83be6492d4976c3b70f9
Ekaterina.Zenina <zeninaekaterin@gmail.com>
2021-11-09 09:58:27 +0300

    v1

---
+++ src/main/java/domain/Vk.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class Vk {
+    private String idOfPost;
+    private String dateOfPost;
+    private String titleOfNews;
+    private String contentOfNews;
+    private String likesOfPost;
+    private String commentsInfo;
+    private String imageUrl;
+    private String viewsInfo;
+    private String addToFavourite;
+    private String repostsOfPost;
+    private String pinOfPost;
+    private String postComplaint;
+}
