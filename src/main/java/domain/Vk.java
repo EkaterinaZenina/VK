256c2ffa6d5aad2835129c5a0c013113453d796c
Ekaterina.Zenina <zeninaekaterin@gmail.com>
2021-11-09 10:40:05 +0300

    v1

--- src/main/java/domain/Vk.java
+++ src/main/java/domain/Vk.java
@@ -16,18 +16,4 @@ public class Vk {
     private String deletePost;
     private String editPost;
 
-
-    private String ownerId;
-    private String fromId;
-    private String createdBy;
-    private String replyOwnerId;
-    private String friendsOnly;
-    private String copyright;
-    private String postType;
-    private String postSource;
-    private String geo;
-    private String isPinned;
-    private String markedAsAds;
-    private String donut;
-    private String postponedId;
 }
