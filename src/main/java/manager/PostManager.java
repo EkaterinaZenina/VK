e83ca65d890951911eb0957ab13d548fa6c0c851
Ekaterina.Zenina <zeninaekaterin@gmail.com>
2021-11-10 17:44:11 +0300

    init

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,15 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+        private Post[] posts;
+
+        public Post[] search (int ownerId,  int domain, int query, int ownersOnly, int count, int offset){
+            return null;
+        }
+        public void delete (int ownerId, int postId ){
+        }
+
+    }
+
